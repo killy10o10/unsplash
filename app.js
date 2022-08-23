fad4dad708cbd5c91b223c0dd6840a82068293c1
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-08-23 16:00:13 +0000

    add crypto info

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 const body = document.querySelector('body');
 const author = document.querySelector('#image-author');
+const cryptoInfo = document.querySelector('#crypto-info');
+const form = document.querySelector('#form');
 
-fetch(
-  'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature'
-)
-  .then((res) => res.json())
-  .then((data) => {
+// Get Background from Unsplash
+const background = async () => {
+  const response = await fetch(
+    'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query'
+  );
+  const data = await response.json();
+  body.style.backgroundImage = `url('${data.urls.full}')`;
+  author.innerText = `Image Credit: ${data.user.name}`;
+  if (data.urls.full) {
     console.log(data.urls.full);
-    body.style.backgroundImage = `url('${data.urls.full}')`;
-    author.innerText = `Image Credit: ${data.user.name}`;
-  });
+  }
+};
+
+background().catch(() => {
+  body.style.backgroundImage = `url('./953897.jpg')`;
+  console.error("sorry couldn't fetch the image😔");
+});
+
+// Get Crypto info on page load
+fetch('https://api.coingecko.com/api/v3/coins/internet-computer')
+  .then((res) => {
+    if (!res.ok) {
+      throw Error('Something went wrong');
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    cryptoInfo.innerHTML = `<img src="${data.image.small}" alt="dogecoin"> <span>${data.name}</span>`;
+  })
+  .catch((err) => console.error(err));
+
+// Search for crypto info
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const search = document.querySelector('#search').value;
+  fetch(`https://api.coingecko.com/api/v3/coins/${search}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw Error('Something went wrong');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      cryptoInfo.innerHTML = `<img src="${data.image.small}" alt="dogecoin"> <span>${data.name}</span>`;
+    })
+    .catch((err) => console.error(err));
+});
+
+// Display Time
+const tick = () => {
+  const now = new Date();
+  const hour = now.getHours() === 0 ? `0${now.getHours()}` : now.getHours();
+  const min = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+  const timeIndicator = document.getElementById('time');
+
+  timeIndicator.innerHTML = `${hour}:${min}`;
+};
+
+setInterval(tick, 1000);
